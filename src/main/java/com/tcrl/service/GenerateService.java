ab4e0fb2af992c58d849d4f87d749483caaca45f
taoxia <781775099@qq.com>
2019-11-22 15:34:27 +0800

    11.21增加了代码生成功能。

---
+++ src/main/java/com/tcrl/service/GenerateService.java
@@ -0,0 +1,35 @@
+package com.tcrl.service;
+
+
+
+
+import com.tcrl.dto.BeanField;
+import com.tcrl.dto.GenerateInput;
+
+import java.util.List;
+
+public interface GenerateService {
+
+	/**
+	 * 获取数据库表信息
+	 * 
+	 * @param tableName
+	 * @return
+	 */
+	List<BeanField> listBeanField(String tableName);
+
+	/**
+	 * 转成驼峰并大写第一个字母
+	 * 
+	 * @param string
+	 * @return
+	 */
+	String upperFirstChar(String string);
+
+	/**
+	 * 生成代码
+	 * 
+	 * @param input
+	 */
+	void saveCode(GenerateInput input);
+}
